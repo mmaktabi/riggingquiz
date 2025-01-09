f9ece8c413b129e3224983853c6d5cc6207b8669
mmaktabi <online@mmaktabi.com>
2025-01-09 08:20:24 +0100

    scrollsilver

--- lib/utils/layout.dart
+++ lib/utils/layout.dart
@@ -4,13 +4,12 @@ import 'package:rigging_quiz/utils/images.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:rigging_quiz/widgets/score/score_header.dart';
 
-/// Diese Klasse enthält das Layout mit `Scaffold` und einer verbesserten Scroll-Implementierung
+/// Diese Klasse enthält das Layout mit `Scaffold` und einer verbesserten Scroll-Implementierung mittels Slivers
 class QLayout extends StatefulWidget {
   final Widget child;
   final bool backButton;
   final bool showScore;
   final bool addEmptyHeader;
-  final bool noScroll;
 
   // Maximalbreite für den Inhalt
   final double maxWidth;
@@ -24,7 +23,6 @@ class QLayout extends StatefulWidget {
     this.showScore = false,
     this.addEmptyHeader = false,
     this.maxWidth = 1000,
-    this.noScroll = false,
   });
 
   @override
@@ -37,66 +35,69 @@ class _QLayoutState extends State<QLayout> {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     double containerWidth = width > widget.maxWidth ? widget.maxWidth : width;
+    double containerPadding = width > widget.maxWidth ? width * 0.15 : 0;
 
     return Scaffold(
-      resizeToAvoidBottomInset: true, // Anpassung bei der Tastatur
-      body: Center(
-        child: Container(
-          decoration: const BoxDecoration(
-            boxShadow: [
-              BoxShadow(
-                color: Color.fromRGBO(0, 49, 94, 0.5),
-                spreadRadius: 2,
-                blurRadius: 5,
-                offset: Offset(0, 1),
-              )
-            ],
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              stops: [0.0, 1.0, 1.0],
-              colors: [
-                QColors.primaryColor,
-                QColors.backgroundColor,
-                QColors.backgroundColor,
-              ],
+      body: CustomScrollView(
+        controller: widget.scrollController,
+        physics: const BouncingScrollPhysics(),
+        slivers: [
+          SliverPadding(
+            padding: EdgeInsets.symmetric(horizontal: containerPadding),
+            sliver: SliverToBoxAdapter(
+              child: Container(
+                constraints: BoxConstraints(minHeight: height),
+                width: containerWidth,
+                decoration: const BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromRGBO(0, 49, 94, 0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 1),
+                    )
+                  ],
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: [0.0, 1.0, 1.0],
+                    colors: [
+                      QColors.primaryColor,
+                      QColors.backgroundColor,
+                      QColors.backgroundColor,
+                    ],
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    // Back-Button als Header
+                    if (widget.backButton)
+                      AppBar(
+                        leading: IconButton(
+                          icon: const Icon(Icons.arrow_back,
+                              size: 30, color: QColors.white),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        ),
+                        backgroundColor: Colors.transparent,
+                        elevation: 0,
+                      ),
+                    // Optionaler leerer Header
+                    if (widget.addEmptyHeader)
+                      const SizedBox(height: 50),
+                    // Score-Header anzeigen
+                    if (widget.showScore) const ScoreHeader(badges: []),
+                    // Der Hauptinhalt
+                    widget.child,
+                    const SizedBox(height: 200),
+                  ],
+                ),
+              ),
             ),
           ),
-          constraints: BoxConstraints(minHeight: height),
-          width: containerWidth,
-          child: widget.noScroll
-              ? widget.child
-              : ListView(
-            controller: widget.scrollController ,
-            physics: const BouncingScrollPhysics(), // Verbesserte Scroll-Physik
-            padding: EdgeInsets.zero,
-            children: [
-              // Back-Button als Header
-              if (widget.backButton)
-                AppBar(
-                  leading: IconButton(
-                    icon: const Icon(Icons.arrow_back,
-                        size: 30, color: QColors.white),
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                  backgroundColor: Colors.transparent,
-                  elevation: 0,
-                ),
-              // Optionaler leerer Header
-              if (widget.addEmptyHeader)
-                const SizedBox(height: 50),
-              // Score-Header anzeigen
-              if (widget.showScore)
-                const ScoreHeader(badges: []),
-              // Der Hauptinhalt
-              widget.child,
-              // Platz am Ende für Polsterung
-              const SizedBox(height: 100),
-            ],
-          ),
-        ),
+          // Sie können weitere Sliver hinzufügen, falls erforderlich.
+        ],
       ),
     );
   }
